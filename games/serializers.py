4e7f02ddcaaac259979762e6692dcc1592f5165a
ezra <ezrayeneneh1992@gmail.com>
2023-07-17 10:31:00 +0300

    did features of signed or not

--- games/serializers.py
+++ games/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers, permissions
-from rest_framework.permissions import IsAuthenticated
 
 from .models import VideoGame, Video, Review, Rating, WatchList, Photo, Trivia, Goof, Quote, FrequentlyAskedQuestion, ParentsGuide, Help
 
@@ -26,15 +25,12 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = ('review',)
 class RatingSerializer(serializers.ModelSerializer):
-    permission_classes = [IsAuthenticated]
     class Meta:
         model = Rating
         fields = ('your_rating',)
 class WatchListSerializer(serializers.ModelSerializer):
-    permission_classes = [IsAuthenticated]
     class Meta:
         model = WatchList
-        permission_classes = [IsAuthenticated]
         fields = ('your_watchlist',)
 
 class TriviaSerializer(serializers.ModelSerializer):
@@ -64,6 +60,18 @@ class HelpListSerializer(serializers.ModelSerializer):
 
 
 class GameDetailSerializer(serializers.ModelSerializer):
+    videos = VideoSerializer(many=True, read_only=True)
+    photos = PhotoSerializer(many=True, read_only=True)
+    class Meta:
+        model = VideoGame
+        fields = '__all__'
+    reviews = ReviewSerializer(many=True, read_only=True)
+    trivias = TriviaSerializer(many=True, read_only=True)
+    goofs = GoofSerializer(many=True, read_only=True)
+    quotes = QuoteSerializer(many=True, read_only=True)
+    faqs = FrequentlyAskedQuestionSerializer(many=True, read_only=True)
+    certificate = ParentsGuideSerializer(many=True, read_only=True)
+class GameDetailSignedSerializer(serializers.ModelSerializer):
     ratings = RatingSerializer(many=True, read_only=True)
     videos = VideoSerializer(many=True, read_only=True)
     photos = PhotoSerializer
@@ -77,3 +85,19 @@ class GameDetailSerializer(serializers.ModelSerializer):
     quotes = QuoteSerializer(many=True, read_only=True)
     faqs = FrequentlyAskedQuestionSerializer(many=True, read_only=True)
     certificate = ParentsGuideSerializer(many=True, read_only=True)
+
+class GameListSignedSerializer(serializers.ModelSerializer):
+    ratings = RatingSerializer(many=True, read_only=True)
+    watchlists = WatchListSerializer(many=True, read_only=True)
+    class Meta:
+        fields = (
+                "title",
+                "cover",
+                "release_date",
+                "imdb_rating",
+                "certificate",
+                "ratings",
+                "watchlists",
+        )
+        model = VideoGame
+
--- games/views.py
+++ games/views.py
@@ -1,24 +1,28 @@
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
 
-from .models import VideoGame, Help, Rating
-from .serializers import GameListSerializer, GameDetailSerializer, HelpListSerializer, RatingSerializer
+from .models import VideoGame, Help
+from .serializers import GameListSerializer, GameDetailSerializer, HelpListSerializer, GameDetailSignedSerializer, GameListSignedSerializer
 
 # Create your views here.
 
-class GameSigned(generics.RetrieveAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = Rating.objects.all()
-    serializer_class = RatingSerializer(many=True, read_only=True)
-
 class GameList(generics.ListAPIView):
     queryset = VideoGame.objects.all()
-    serializer_class = GameListSerializer
+    
+    def get_serializer_class(self):
+        if self.request.user.is_authenticated:
+            return GameListSignedSerializer
+        else:
+            return GameListSerializer
 
 class GameDetail(generics.RetrieveAPIView):
     queryset = VideoGame.objects.all()
-    serializer_class = GameDetailSerializer
-    my_ratings = GameSigned.as_view()
+
+    def get_serializer_class(self):
+        if self.request.user.is_authenticated:
+            return GameDetailSignedSerializer
+        else:
+            return GameDetailSerializer
 
 class HelpList(generics.ListCreateAPIView):
     queryset = Help.objects.all()
